222b03d3e314f7f9984a64678c58d58c1f254e0c
TH0RlN <th0rln@github.com>
2021-01-12 18:21:31 +0100

    No fuca

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ def main():
 	q = f"track:{input('Búsqueda: ')}"
 	spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-	results = spotify.search(q, limit=50, offset=0, type='track', market=None)
-#	print(results)
-	tracks = results['tracks']['items']
-	
-	
-	for track in tracks:
-		print(track['name'])
-		#print("\n\n\n")
-	spotify.user_playlist_create("raul_ubl6", "lista de prueba", public=True, collaborative=False, description='')
+	results = spotify.search(q, limit=50, offset=0, type='track', market='ES')
+	print(results)
+#	tracks = results['tracks']['items']
+#	
+#	
+#	for track in tracks:
+#		print(track['name'])
+#		#print("\n\n\n")
+	#spotify.user_playlist_create("raul_ubl6", "lista de prueba", public=True, collaborative=False, description='')
 
 
 
